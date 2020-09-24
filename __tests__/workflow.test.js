c6affb7906d29bc59c689975a64ceeb6cb92c56b
Gianluca Esposito <g@esposi.to>
2020-09-24 09:38:16 +0200

    Add support for custom Workflow jobs reporter

---
+++ __tests__/workflow.test.js
@@ -0,0 +1,17 @@
+const { failingCasesFrom, annotationsFrom } = require("../index");
+
+// Custom JUnit report built from Workflow jobs with
+// https://github.com/gesposito/action-workflow-jobs-reporter
+
+describe("Workflow report", () => {
+  let failingCases;
+  test("Converts JUnit XML to failing cases in JSON", async () => {
+    failingCases = await failingCasesFrom("./test-results/workflow-jobs.xml");
+    expect(failingCases).toMatchSnapshot();
+  });
+
+  test("Converts failing cases in JSON to GitHub annotations", async () => {
+    const annotations = annotationsFrom(failingCases);
+    expect(annotations).toMatchSnapshot();
+  });
+});
